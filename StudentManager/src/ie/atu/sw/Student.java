6d44d5d220b7858fa3e2c0c0b83531cf6796f7e9
siog <siog@LAPTOP-545TJ6PE>
2025-05-19 17:45:41 +0100

    Use source to generate getters and setters

--- StudentManager/src/ie/atu/sw/Student.java
+++ StudentManager/src/ie/atu/sw/Student.java
@@ -25,4 +25,55 @@ public class Student {
 		this.dob = dob;
 	}
 	
+	// Use source to generate getters and setters
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	
+	
+	
 }
