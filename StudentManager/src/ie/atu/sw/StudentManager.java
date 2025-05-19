bce8ce689f92a64990b636dd8c6413d23bc75a1c
siog <siog@LAPTOP-545TJ6PE>
2025-05-19 19:26:02 +0100

    Add methods to the StudentManager class

--- StudentManager/src/ie/atu/sw/StudentManager.java
+++ StudentManager/src/ie/atu/sw/StudentManager.java
@@ -4,4 +4,24 @@ public class StudentManager {
 	private static final int INITIAL_CAPACITY = 10;
 	private Student[] student = null;
 	
+	public boolean add(Student s) {
+		return false;
+	}
+	
+	public boolean delete(String sid) {
+		return false;
+	}
+	
+	public Student getStudentByID(String sid) {
+		return null;
+	}
+	
+	public Student[] getStudentsByFirstname(String firstname) {
+		return null;
+	}
+	
+	public int size() {
+		return 0;
+	}
 }
+
