92550f72a0c780895a56f8e5fa7123a41acb5eda
siog <siog@LAPTOP-545TJ6PE>
2025-05-25 15:15:32 +0100

    Add print statements to create menu

--- StudentManager/src/ie/atu/sw/Menu.java
+++ StudentManager/src/ie/atu/sw/Menu.java
@@ -4,11 +4,20 @@ package ie.atu.sw;
  * This class will do nothing more than display the menu
  * and get input from the user
  */
-
+import static java.lang.System.*;
 public class Menu {
 	
 	private void showOptions() {
-		
+		out.print("******************************");
+		out.print("**    Student Manager 1.0   **");
+		out.print("******************************");
+		out.print("(1) Add a Student");
+		out.print("(2) Delete a Student");
+		out.print("(3) Find Student by ID");
+		out.print("(4) Find Students by Firstname");
+		out.print("(5) Get Total Student Number");
+		out.print("(6) Quit");
+		out.print("Select an Option [1 - 6]>");
 	}
 	
 }
