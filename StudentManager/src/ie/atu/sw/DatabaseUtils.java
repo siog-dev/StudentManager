d64cab002ba5cea6d72e7056581b369c01b4f824
siog <siog@LAPTOP-545TJ6PE>
2025-06-10 16:30:59 +0100

    Create save and load methods

--- StudentManager/src/ie/atu/sw/DatabaseUtils.java
+++ StudentManager/src/ie/atu/sw/DatabaseUtils.java
@@ -3,15 +3,25 @@ package ie.atu.sw;
 import java.io.*;
 public class DatabaseUtils {
 
-	public void save(StudentManager sm, String fileName) {
+	public static void save(StudentManager sm, String fileName) throws Exception {
 		try {
 			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(fileName)));
 			out.writeObject(sm);
 			out.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new Exception("[Error] Cannot save the Student Manager to " + fileName + ". " + e.getMessage());
 		}
 	}
 	
+	// Instead of taking in a StudentManager, return a StudentManager
+	public static StudentManager load(String fileName) throws Exception {
+		try {
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(fileName)));
+			StudentManager sm = (StudentManager) in.readObject();
+			in.close();
+			return sm;
+		} catch (IOException e) {
+			throw new Exception("[Error] Cannot load the Student Manager from " + fileName + ". " + e.getMessage());
+		}
+	}
 }
